c23fa2a1f00c028d0a6dc36a499c52e54c3d2082
HirlanFerreira <hirlanreis17@gmail.com>
2025-01-21 20:55:05 -0300

    aula de operador logico not !

---
+++ 17_operador_lógico_not/js/scripts.js
@@ -0,0 +1,14 @@
+/*Operdor lógico not 
+O operador lógico ! é conhecidotambém como NOT;
+Este valor muda o valor que a experessão retornou;
+Se recebeu true vira false, se recebeu false vira true; */
+
+if (!false) {
+  console.log('Passou');
+}
+
+var nome = "Hirlan";
+
+if (!(nome == "Daniel")) {
+  console.log("ok");
+}
